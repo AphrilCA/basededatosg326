6c1929b61d717d6f48a0b2c793b040e4769d7e0a
Ivonneca <aphrilca9@gmail.com>
2026-07-08 09:58:57 -0600

    Se realizo la codificacion de las reestricciones UNIQUE y DEFAULT  de las diferentes formas de construcción en las tablas (nombradas y no nombradas), se continua posteriormente con la restriccion CHECK

--- 03-construccion-basesdedatos/01-lenguajedefiniciondatos.sql
+++ 03-construccion-basesdedatos/01-lenguajedefiniciondatos.sql
@@ -1,4 +1,4 @@
---Construccion de Bases de datos con SQL-LDD (Create, Alter, Drop)
+-- construcción de bases de datos con SQL-LDD (Create, Alter, Drop)
 
 -- Crear una base de datos
 
@@ -10,110 +10,117 @@ USE universidad;
 GO
 
 -- Crear una tabla
-CREATE TABLE alumno (
+
+CREATE TABLE alumno(
 	alumno_id INT,
-	nombre VARCHAR (50),
+	nombre_alumno VARCHAR (50),
 	apellido_paterno VARCHAR (20),
 	apellido_materno VARCHAR (20),
-	fecha_nacimiento DATE, 
-	correo VARCHAR (50) 
+	fecha_nacimiento DATE,
+	correo VARCHAR (50)
 );
 GO
-
-
 
 INSERT INTO alumno
 VALUES (1, 'Arcadia', 'Vaca', 'Del Corral', '1986-04-07', 'correo@correo.com');
 
 INSERT INTO alumno
-VALUES (1, 'Kevin', 'Cabeza', 'De Borrego', '1942-05-10', 'correo@correo.com');
+VALUES (1, 'Kevin', 'Cabeza', 'Del Borreo', '1942-04-07', 'correo@correo.com');
 
 SELECT *
 FROM alumno;
 
--- Restricciones 
-/* Las restricciones son reglas que garantizan la integridad de los datos
- Las mas utilizadas son:
- - PRIMARY KEY
- - FOREGIN KEY
- - NOT NULL
- - UNIQUE
- - CHECK
- - DEFAULT
+-- Renstricciones
+/*
+	Las renstricciones son reglas que garantizan la integridad de los datos
 
-*/ 
+	Las más utilizadas son:
+	
+	- PRIMARY KEY
+	- FOREIGN KEY
+	- NOT NULL
+	- UNIQUE
+	- CHECK
+	- DEFAULT
+*/
 
 
+DROP TABLE alumno;
 GO
 
--- PRIMARY KEY
+ -- PRIMARY KEY
 
-CREATE TABLE alumno (
-alumno_id INT PRIMARY KEY,
-nombre VARCHAR (50),
-correo VARCHAR (50)
-
+CREATE TABLE alumno(
+	alumno_id INT PRIMARY KEY,
+	nombre VARCHAR(50),
+	correo VARCHAR(50)
 );
 GO
 
 INSERT INTO alumno
-VALUES (1, 'Luis', 'correo@Luis.com' );
+VALUES (1, 'Luis', 'correo@correo');
 
-INSERT INTO alumno 
-VALUES (1, 'Roberta', 'correo@Roberta.com' );
+
+INSERT INTO alumno
+VALUES (2, 'Jose', 'correo@correo');
+
+DROP TABLE alumno;
+GO
+
+
+CREATE TABLE alumno(
+	alumno_id INT NOT NULL,
+	nombre VARCHAR(50),
+	correo VARCHAR(50)
+	CONSTRAINT pk_alumno
+	PRIMARY KEY (alumno_id)
+);
+GO
 
 DROP TABLE alumno;
 
-CREATE TABLE alumno (
-alumno_id INT PRIMARY KEY,
-nombre VARCHAR (50),
-correo VARCHAR (50),
-CONSTRAINT pk_alumno
-PRIMARY KEY (alumno_id)
-);
-GO
-
-INSERT INTO alumno
-VALUES (1, 'Luis', 'correo@Luis.com' );
-
-INSERT INTO alumno 
-VALUES (1, 'Roberta', 'correo@Roberta.com' );
+-- PRIMARY KEY CON IDENTITY
 
 DROP TABLE alumno;
+GO
 
---PRIMARY KEY CON IDENTITY
 
-CREATE TABLE alumno (
-alumno_id INT IDENTITY (1,1) PRIMARY KEY,
-nombre VARCHAR (50),
-correo VARCHAR (50),
+CREATE TABLE alumno(
+	alumno_id INT IDENTITY (1,1) PRIMARY KEY,
+	nombre VARCHAR(50),
+	correo VARCHAR(50)
 );
 GO
 
 INSERT INTO alumno
-VALUES (1, 'Luis', 'correo@Luis.com' );
+VALUES ('Luis', 'correo@correo');
 
-INSERT INTO alumno 
-VALUES (1, 'Roberta', 'correo@Roberta.com' );
 
-SELECT + FROM alumno;
+INSERT INTO alumno
+VALUES ('Jose', 'correo@correo');
 
-CREATE TABLE alumno (
-alumno_id INT NOT NULL IDENTITY (1, 1),
-nombre VARCHAR (50),
-correo VARCHAR (50),
-CONSTRAINT pk_alumno,
-PRIMARY KEY (alumno_id)
+SELECT * FROM alumno;
+
+DROP TABLE alumno;
+GO
+
+CREATE TABLE alumno(
+	alumno_id INT NOT NULL IDENTITY (1,1),
+	nombre VARCHAR(50),
+	correo VARCHAR(50)
+	CONSTRAINT pk_alumno
+	PRIMARY KEY (alumno_id)
 );
 GO
 
--- NOT NULL 
-CREATE TABLE profesor(
+-- NOT NULL
+
+CREATE TABLE profesor (
 	profesor_id INT NOT NULL IDENTITY (1,1),
 	numero_nomina VARCHAR (20) NOT NULL,
 	nombre VARCHAR (15) NOT NULL,
-	apellido_materno VARCHAR (20) NOT NULL,
-	apellido_paterno VARCHAR (20) NULL,
+	apellido_paterno VARCHAR (20) NOT NULL,
+	apellido_materno VARCHAR (20) NULL,
 	fecha_ingreso DATE,
 	CONSTRAINT pk_profesor
 	PRIMARY KEY (profesor_id)
@@ -121,13 +128,74 @@ CREATE TABLE profesor(
 GO
 
 INSERT INTO profesor
-VALUES ('155151S', 'Luis', 'Hernandez', NULL,  '2027-05-07');
-
-
-SELECT *
-FROM profesor;
+VALUES ('55555555','Jose','Hernandez',NULL, '2027-01-01');
 
 INSERT INTO profesor (numero_nomina, nombre, apellido_materno)
-VALUES ('DFSF', 'Ricarda', 'Sonrics')
+VALUES ('2222222', 'Ricarda', 'Sonrics');
 
--- Restriccion UNIQUE
+-- Renstricción UNIQUE
+
+CREATE TABLE categoria(
+categoria_id INT NOT NULL PRIMARY KEY IDENTITY (1,1),
+nombe VARCHAR (20) NOT NULL UNIQUE,
+activo BIT NOT NULL
+);
+GO
+
+INSERT INTO categoria
+VALUES (UPPER('carnes firas'), 1);
+
+
+INSERT INTO categoria
+VALUES (UPPER('carnes firas'), 1);
+
+DROP TABLE categoria;
+GO
+
+
+CREATE TABLE categoria(
+categoria_id INT NOT NULL IDENTITY (1,1)
+CONSTRAINT pk_categoria
+PRIMARY KEY (categoria_id),
+nombre VARCHAR (20) NOT NULL
+CONSTRAINT uq_categoria_nombre
+UNIQUE,
+activo BIT NOT NULL
+
+);
+GO
+
+DROP TABLE categoria;
+GO
+
+
+CREATE TABLE categoria(
+categoria_id INT NOT NULL IDENTITY (1,1)
+nombre VARCHAR (20) NOT NULL,
+activo BIT NOT NULL,
+CONSTRAINT pk_categoria
+PRIMARY KEY (categoria_id),
+activo BIT NOT NULL
+CONSTRAINT uq_categoria_nombre
+UNIQUE (nombre)
+
+-- Restriccion DEFAULT
+
+CREATE TABLE categoria(
+);
+GO
+
+
+INSERT INTO categoria (nombre, activo)
+VALUES ('carnes frias') DEFAULT);
+
+
+INSERT INTO categoria (nombre, activo)
+VALUES ('Lacteos') DEFAULT);
+
+SELECT *
+FROM categoria;
+
+-- TODO: CREAR LAS TABLAS DE LAS OTRAS DOS FORMAS 
+
+-- TODO: CHECK
